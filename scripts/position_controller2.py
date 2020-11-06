b8831b898fda744d93ca5d19272ef4c89b1b8f75
Ekansh Gupta <ekanshgupta92@gmail.com>
2020-11-06 12:37:31 +0530

    fixed Position_controller2.py

--- scripts/position_controller2.py
+++ scripts/position_controller2.py
@@ -34,27 +34,24 @@ class EdroneController():
         self.cmd_drone.rcYaw = 1500
 
         self.max_values =[2000, 2000, 2000, 2000]
-        self.min_values =[1000, 1000, 1000, 1000]
+        self.min_values =[1450, 1450, 1450, 1450]
 
-        self.final_setpoint = [19.0000451704 , 72.0, 0, 3] # latitude, longitude, yaw, altitude
-        self.current_point = [19.0, 72.0, 0, 0.31] # given startpoint
+        self.final_setpoint = [19.0000451704 , 72.0, 3] # latitude, longitude, altitude
+        self.current_point = [19.0, 72.0, 0.31] # given startpoint
 
-        self.setpoint_error = [0, 0, 0, 0] # roll, pitch, yaw, throttle
+        self.setpoint_error = [0, 0, 0] # roll, pitch, throttle
         
-        self.error = [0, 0, 0, 0] # roll, pitch, yaw, throttle
-        self.sum_error = [0, 0, 0, 0]
-        self.prev_error = [0, 0, 0, 0]
+        self.error = [0, 0, 0] # roll, pitch, throttle
+        self.sum_error = [0, 0, 0]
+        self.prev_error = [0, 0, 0]
         
         self.lattitude = 0
         self.longitude = 0
         self.altitude = 0 
-        # self.pid_values_roll = [4.02, 0, 2.4] # Kp, Ki, Kd
-        # self.pid_values_pitch = [1.02, 0, 1.2] # Kp, Ki, Kd
-        # self.pid_values_yaw = [82.56, 0, 0] # Kp, Ki, Kd
-        #self.pid_values_throttle = [0, 0, 0] # Kp, Ki, Kd
-        self.Kp = [4000000, 50, 0, 38.4]  # latitude, longitude, yaw, altitude
-        self.Ki = [0, 0.32, 0, 0]
-        self.Kd = [5000000, 80, 0, 90.6]
+
+        self.Kp = [400000, 40, 50]  # latitude, longitude, altitude
+        self.Ki = [0, 0, 0.32]
+        self.Kd = [500000, 50, 80]
 
         # Publisher
         self.cmd_pub = rospy.Publisher('/drone_command', edrone_cmd, queue_size=1)
@@ -85,23 +82,25 @@ class EdroneController():
         self.Kd[1] = pitch.Kd * 0.3
 
     def yaw_set_pid(self, yaw):
-        self.Kp[3] = yaw.Kp * 0.06  # This is just for an example. You can change the ratio/fraction value accordingly
-        self.Ki[3] = yaw.Ki * 0.008
-        self.Kd[3] = yaw.Kd * 0.3
+        self.Kp[2] = yaw.Kp * 100.  # This is just for an example. You can change the ratio/fraction value accordingly
+        self.Ki[2] = yaw.Ki * 0.0001
+        self.Kd[2] = yaw.Kd * 100.
 
     def altitude_control(self):
-        self.error[3] = self.final_setpoint[3] - self.current_point[3]
-        self.sum_error[3] += self.error[3]
-        self.setpoint_error[3] = (self.error[3]*self.Kp[3]) + (self.Ki[3] * self.sum_error[3]) + (self.Kd[3] * (self.error[3] - self.prev_error[3])/self.sample_time)
-        self.prev_error[3] = self.error[3]
-        self.cmd_drone.rcThrottle = 1500 + self.setpoint_error[3]
-        self.cmd_drone.rcThrottle = max(min(self.max_values[3], self.cmd_drone.rcThrottle), self.min_values[3])
+        self.error[2] = self.final_setpoint[2] - self.current_point[2]
+        self.sum_error[2] = self.sum_error[2] + self.error[2]
+        self.setpoint_error[2] = (self.error[2]*self.Kp[2]) + (self.Ki[2] * self.sum_error[2]) + (self.Kd[2] * (self.error[2] - self.prev_error[2])/self.sample_time)
+        self.prev_error[2] = self.error[2]
+        self.cmd_drone.rcThrottle = 1500 + self.setpoint_error[2]
+        self.cmd_drone.rcThrottle = max(min(self.max_values[2], self.cmd_drone.rcThrottle), self.min_values[2])
         self.cmd_pub.publish(self.cmd_drone)
-        self.error_yaw_pub.publish(self.error[3])
+        self.error_roll_pub.publish(self.error[0])
+        self.error_pitch_pub.publish(self.error[1])
+        self.error_yaw_pub.publish(self.error[2])
 
     def coordinate_control(self):
         self.error[0] = self.final_setpoint[0] - self.current_point[0]
-        self.sum_error[0] += self.error[0]
+        self.sum_error[0] = self.sum_error[0] + self.error[0]
         self.setpoint_error[0] = (self.error[0]*self.Kp[0]) + (self.Ki[0] * self.sum_error[0]) + (self.Kd[0] * (self.error[0] - self.prev_error[0])/self.sample_time)
         self.prev_error[0] = self.error[0]
         self.cmd_drone.rcRoll = 1500 + self.setpoint_error[0]
@@ -115,22 +114,22 @@ class EdroneController():
         self.cmd_drone.rcPitch = 1500 + self.setpoint_error[1]
         self.cmd_drone.rcPitch = max(min(self.max_values[1], self.cmd_drone.rcPitch), self.min_values[1])
         self.cmd_pub.publish(self.cmd_drone)
+        self.error_roll_pub.publish(self.error[0])
+        self.error_pitch_pub.publish(self.error[1])
+        self.error_yaw_pub.publish(self.error[2])
 
         
 
     def control_pid(self):
-        while self.current_point[3] != self.final_setpoint[3]:
+        while abs(self.final_setpoint[2] - self.current_point[2]) > 0.2 :
             self.altitude_control()
 
-        self.coordinate_control()
-
-        self.error_yaw_pub.publish(self.error[3])
-        self.error_roll_pub.publish(self.error[0])
-        self.error_pitch_pub.publish(self.error[1])
-
-        if self.current_point[0] == self.final_setpoint[0] and self.current_point[1] == self.final_setpoint[1]:
-            self.final_setpoint[3] == 0.31
-            self.altitude_control()
+        t_end = time.time() + 30 
+        while time.time() < t_end:
+            self.coordinate_control()
+            
+        self.final_setpoint[2] == 0.31
+        self.altitude_control()
             
         
         
